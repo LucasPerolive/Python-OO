dcb67d3ec8d226efdec6e648e0c904684f66c050
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-04-11 14:12:03 -0300

    Add files via upload

---
+++ OO/conta.py
@@ -0,0 +1,63 @@
+"""
+from conta import Conta
+conta = Conta(123, "Lucas", 55.5, 1000.0)
+conta2 = Conta(321, "Clay", 100.0, 1000.0)
+
+"""
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        self.__codigo_banco = "001"
+
+    def extrato(self):
+        print ("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_saque = self.__limite + self.__saldo
+        return valor_a_sacar <= valor_disponivel_saque
+
+    def saca(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def trasfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
